b1b3be24a6b188af848a45b40c85a4e1a682d72a
jimmitjoo <jimmitjoo@gmail.com>
2014-07-07 17:18:12 +0200

    Username fix

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -135,7 +135,12 @@ class UsersController extends \BaseController {
             $u = User::where('email', '=', $result['email'])->first();
             if (!$u) $u = User::where('facebook_identification', '=', $result['id'])->first();
 
-            if (!$u->username || empty($u->username)) $u->username = $result['first_name']. ' ' . $result['last_name'];
+
+            if (!$u->username || empty($u->username)) {
+                $u->username = $result['first_name']. ' ' . $result['last_name'];
+                $u->save();
+            }
+
 
             Auth::login($u);
 
