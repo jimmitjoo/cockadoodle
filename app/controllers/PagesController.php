02bbe19a4cccf9f692fb80241b4bd91c995b36e2
jimmitjoo <jimmitjoo@gmail.com>
2014-07-08 14:28:27 +0200

    Fix some pages names

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -3,83 +3,23 @@
 class PagesController extends \BaseController {
 
 	/**
-	 * Display the startpage.
+	 * Display the first page.
 	 *
 	 * @return Response
 	 */
-	public function startpage()
+	public function start()
 	{
 		return View::make('hello');
 	}
 
 	/**
-	 * Show the form for creating a new resource.
-	 * GET /pages/create
+	 * Display the login page
 	 *
 	 * @return Response
 	 */
 	public function login()
 	{
 		return View::make('login');
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /pages
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
-
-	/**
-	 * Display the specified resource.
-	 * GET /pages/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /pages/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /pages/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /pages/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
 	}
 
 }
--- app/routes.php
+++ app/routes.php
@@ -1,18 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
 // Views
-Route::get('/', 'PagesController@startpage');
+Route::get('/', 'PagesController@start');
 Route::get('/login', 'PagesController@login');
 
 // User resources
@@ -22,4 +11,4 @@ Route::post('/login', 'SessionsController@create');
 
 // Logged in
 Route::get('/fbver', 'UsersController@facebook');
-Route::get('/friendslist', ['as' => 'friends', 'uses' => 'FriendsController@index'])->before('logged_in');
+Route::get('/friends', ['as' => 'friends', 'uses' => 'FriendsController@index'])->before('logged_in');
