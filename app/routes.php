7a5761f896e4b260d2c73e47574c52ad1087e048
jimmitjoo <jimmitjoo@gmail.com>
2014-07-08 14:43:26 +0200

    Fix searchfield for friends

--- app/routes.php
+++ app/routes.php
@@ -12,3 +12,7 @@ Route::post('/login', 'SessionsController@create');
 // Logged in
 Route::get('/fbver', 'UsersController@facebook');
 Route::get('/friends', ['as' => 'friends', 'uses' => 'FriendsController@index'])->before('logged_in');
+
+// Search friends
+Route::post('/api/search', 'FriendsController@search')->before('logged_in');
+//Route::post('/search', 'FriendsController@search')->before('logged_in');
--- app/views/friendslist.blade.php
+++ app/views/friendslist.blade.php
@@ -13,11 +13,25 @@
     @foreach ($friends as $f)
     <li> {{ $f['email'] }} </li>
     @endforeach
+    <li>
+        {{ Form::open('url' => '#', 'method' => 'post') }}
+        {{ Form::text('username', '', ['placeholder' => 'Search for friends', 'class' => 'username_search']) }}
+        {{ Form::close() }}
+    </li>
     </ul>
+
 
     <br /><br /> <a href="/logout">Yeah, or logout here, or whatever you prefer...</a>
 
 @stop
 
 @section('scripts')
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script>
+    $(function() {
+        $('.username_search').on('keyup', function(){
+            alert('key pressed!');
+        });
+    });
+</script>
 @stop
