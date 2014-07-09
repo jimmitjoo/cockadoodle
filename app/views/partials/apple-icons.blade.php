4067a863508fb091a378e8789dc86b8a5aa5c686
jimmitjoo <jimmitjoo@gmail.com>
2014-07-09 17:51:07 +0200

    Fix retarded spelling error

--- app/views/partials/apple-icons.blade.php
+++ app/views/partials/apple-icons.blade.php
@@ -1,10 +1,10 @@
-<link rel="apple-touch-icon" href="/img/icons/AppIcons57x57.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/img/icons/AppIcons72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/img/icons/AppIcons76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/img/icons/AppIcons57x57@2x.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/img/icons/AppIcons60x60@2x.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/img/icons/AppIcons72x72@2x.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/img/icons/AppIcons76x76@2x.png">
+<link rel="apple-touch-icon" href="/img/icons/AppIcon57x57.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/img/icons/AppIcon72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/img/icons/AppIcon76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/img/icons/AppIcon57x57@2x.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/img/icons/AppIcon60x60@2x.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/img/icons/AppIcon72x72@2x.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/img/icons/AppIcon76x76@2x.png">
 
 <!-- Win 8 Tile -->
 <meta name="msapplication-TileImage" content="/img/icons/CaD_Logo_144x144.png">
