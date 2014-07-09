4d31f2938443eaa65d87ebd3e04ee4a0d1bead62
jimmitjoo <jimmitjoo@gmail.com>
2014-07-09 17:49:08 +0200

    Fix with app icons and video

--- app/views/partials/apple-icons.blade.php
+++ app/views/partials/apple-icons.blade.php
@@ -1,12 +1,10 @@
-<link rel="apple-touch-icon" href="/img/icons/CaD_Logo_57x57.png">
-<link rel="apple-touch-icon" sizes="70x70" href="/img/icons/CaD_Logo_70x70png">
-<link rel="apple-touch-icon" sizes="72x72" href="/img/icons/CaD_Logo_72x72png">
-<link rel="apple-touch-icon" sizes="76x76" href="/img/icons/CaD_Logo_76x76png">
-<link rel="apple-touch-icon" sizes="114x114" href="/img/icons/CaD_Logo_114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/img/icons/CaD_Logo_120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/img/icons/CaD_Logo_144x144.png">
-<link rel="apple-touch-icon" sizes="150x150" href="/img/icons/CaD_Logo_150x150.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/img/icons/CaD_Logo_152x152.png">
+<link rel="apple-touch-icon" href="/img/icons/AppIcons57x57.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/img/icons/AppIcons72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/img/icons/AppIcons76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/img/icons/AppIcons57x57@2x.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/img/icons/AppIcons60x60@2x.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/img/icons/AppIcons72x72@2x.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/img/icons/AppIcons76x76@2x.png">
 
 <!-- Win 8 Tile -->
 <meta name="msapplication-TileImage" content="/img/icons/CaD_Logo_144x144.png">
