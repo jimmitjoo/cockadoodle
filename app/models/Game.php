3bce8ed2bf889530e8b97facd97c15aca6cf9a2c
jimmitjoo <jimmitjoo@gmail.com>
2014-07-15 16:47:41 +0200

    added field rules

--- app/models/Game.php
+++ app/models/Game.php
@@ -6,5 +6,10 @@ class Game extends \Eloquent {
         'second_player_id'
     ];
 
+    protected $rules = [
+        'first_player_id' => 'required',
+        'second_player_id' => 'required'
+    ];
+
     protected $table = 'games';
 }
