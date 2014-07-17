45bd268e1c1dfaff4a6f606c36ae49e71bbd7992
jimmitjoo <jimmitjoo@gmail.com>
2014-07-17 12:56:23 +0200

    montserrat added

--- app/views/master/master.blade.php
+++ app/views/master/master.blade.php
@@ -8,7 +8,7 @@
     <title>@yield('title', 'Title')</title>
     @yield('meta','')
 
-    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/montserrat.css"/>
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <link rel="stylesheet" href="css/cockadoodlestyle.css">
 
