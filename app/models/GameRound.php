be137598f50ab7763497954906456f6e60a25d26
jimmitjoo <jimmitjoo@gmail.com>
2014-07-18 11:35:18 +0200

    askdölaskd

--- app/models/GameRound.php
+++ app/models/GameRound.php
@@ -4,10 +4,4 @@ class GameRound extends \Eloquent {
 	protected $fillable = [];
 
     protected $table = 'doodlegames';
-
-
-    public function receiver()
-    {
-        return $this->hasOne('User', 'receiver_id');
-    }
 }
